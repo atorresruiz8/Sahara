d2241925e56a479af8c03da4a436b07938b33b89
graypa1real <graypa1@appstate.edu>
2021-05-28 11:52:49 -0400

    working on product creation

---
+++ Sahara/ProductMaker.swift
@@ -0,0 +1,13 @@
+//
+//  ProductMaker.swift
+//  Sahara
+//
+//  Created by Peter G on 5/28/21.
+//
+
+
+import Foundation
+class ProductCreation{
+    let names = ["Camping Chair", "Grill", "Uninteruptable power source", "macBook Air", "GUCCI handbag", "Playstation 5", "Nonstick Cooking Pan", "Me and the Boys Beans Shirt", "Horse Mask", "Zebra mask", "Re Zero Rem Girl's Anime Body Pillow", "wireless mouse", "SSD External Storage", "Health Mask", "Sad Frog Plushie", "Segway smart self-balance", " Nintendo Switch Pro", "Gaming Office Chair", "Table", "Blue Yeti Microphone", "Mic Stand with Pop Filter", "Dual Monitor Adjustable Vesa Stand", "Florida Man Cooking Book", "Go the F to Sleep by Adam Mansback", "Kiddie Pool for Pets", "The Nut Button Toy", "OK Boomer Button Toy", "Xbox 720", "Soulja Boy Game Console", "SkyTech Shadow Gaming PC"]
+    let OrigPrice = [55.99, 199.00, 55.99, 999.00, 1799.99, 499.99, 27.99, 17.99, 13.95, 13.95,39.85,9.99,114.99,7.80,17.85,539,9999999.99,220,50.00,105.00,25.99,57.99,6.97,12.25,42.99,19.99,14.99,720.69,98.98,1337.99]
+}
