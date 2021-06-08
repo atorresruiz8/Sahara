7e6bd2b7a89296e63ecb4ba266a6f08c7e41a896
Justin <Justin.Horner@revature.net>
2021-06-08 11:10:52 -0500

    Set up wishlist plus cart

--- Sahara/Account/WishListViewController.swift
+++ Sahara/Account/WishListViewController.swift
@@ -7,50 +7,128 @@
 
 import UIKit
 
-class WishListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    let ud = UserDefaults.standard
-    
-    @IBOutlet weak var tableView: UITableView!
-    var user : User?
-    var prod : Product?
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        user = DBHelper.inst.fetchUser(query: ud.string(forKey: "currUser")!)
-        
-        if(user!.wishlist!.count == 0) {
-            return 0
+
+
+class WishListViewController: UIViewController {
+    static var inst = WishListViewController()
+    static var wishListArray: [wishListItem] = []
+    @IBOutlet weak var mainView: UIScrollView!
+    @IBOutlet weak var welcomeMessage: UIButton!
+    var subviewSize = 0
+        let ud = UserDefaults.standard
+        var user : User?
+        var prod : Product?
+    static func displayWishlist(){
+        WishListViewController.wishListArray = []
+        let ud2 = UserDefaults.standard
+        let user2 = DBHelper.inst.fetchUser(query: ud2.string(forKey: "currUser")!)
+        var prod : Product?
+        for i in 0...user2!.wishlist!.count-1{
+            
+            prod = DBHelper.inst.fetchProduct(id: user2!.wishlist![i])
+            WishListViewController.wishListArray.append(wishListItem(name: prod!.name!, price: String(format: "$%.2f", prod!.price * prod!.salePercentage), pImage: prod!.image!, pIndex: i))
+            
         }
         
-        return user!.wishlist!.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! WishListTableViewCell
-        prod = DBHelper.inst.fetchProduct(id: user!.wishlist![indexPath.row])
-        cell.itemName.text = prod!.name!
-        cell.itemPrice.text = String(format: "$%.2f", prod!.price * prod!.salePercentage)
-        cell.itemImg.image = UIImage(named: prod!.image!)
-        return cell
-    }
-    
+    class wishListItem : CustomView{
+        let ud = UserDefaults.standard
+        var prod : Product?
+        var WishListItemName: String
+        var WishListItemPrice: String
+        var prodImage : String
+        var index: Int
+        init(name: String, price: String, pImage : String, pIndex: Int) {
+            WishListItemName = name
+            WishListItemPrice = price
+            prodImage = pImage
+            index = pIndex
+            super.init(frame: CGRect(x: 50, y: 100, width: UIScreen.main.bounds.width - 100, height: 100))
+            let title = UILabel()
+            title.text = WishListItemName
+            self.addSubview(title)
+            let image = UIImageView(image:UIImage(named: prodImage))
+            image.frame = CGRect(x: 0, y: 22.5, width: 100, height: 100)
+            self.addSubview(image)
+            let delete = CustomButton()
+            delete.setTitle("Delete", for: .normal)
+            delete.frame = CGRect(x: 170, y: 75, width: 70, height: 50)
+            delete.tag = index
+            
+            delete.addTarget(self, action: #selector(deleteWishListItem), for: .touchUpInside)
+            self.addSubview(delete)
+            let price = UILabel()
+            price.text = (String(WishListItemPrice))
+            price.frame = CGRect(x: 270, y: 60, width: 100, height: 20)
+            self.addSubview(price)
+            title.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
 
-    override func viewDidLoad() {
+        @objc func deleteWishListItem(sender: UIButton){
+            let user1 = DBHelper.inst.fetchUser(query: ud.string(forKey: "currUser")!)
+            prod = DBHelper.inst.fetchProduct(id: user1!.wishlist![sender.tag])
+            DBHelper.inst.removeFromWishlist(prodID: prod!.id!, uName: ud.string(forKey: "currUser")!)
+            wishListArray.remove(at: index)
+            WishListViewController.inst.closeView()
+        }
+    }
+    func closeView(){
+        var sb = UIStoryboard(name: "Account", bundle: nil)
+        let wish = sb.instantiateViewController(withIdentifier: "Wish") as! WishListViewController
+        wish.modalPresentationStyle = .fullScreen
+        present(wish, animated: true, completion: nil)
+    }
+    func configureSubView(){
+        addWishlistItemsToSubView()
+        
+    }
+    override func viewDidLoad() {//        testCart()
+        
         super.viewDidLoad()
+        
+        WishListViewController.inst = self
         user = DBHelper.inst.fetchUser(query: ud.string(forKey: "currUser")!)
         
-        if(user?.wishlist == nil){
-            self.tableView.isHidden = true
+        if (ud.string(forKey: "currUser")!.hasPrefix("_")) {
+            welcomeMessage.setTitle("Guest Wishlist", for: UIButton.State.normal)
+        } else {
+            welcomeMessage.setTitle("Hi, \(String(describing: user!.name!))", for: UIButton.State.normal)
+        }
+        WishListViewController.displayWishlist()
+        configureSubView()
+    }
+
+    @IBAction func returnToProfile(_ sender: Any) {
+        var sb = UIStoryboard(name: "Main", bundle: nil)
+
+        let account = sb.instantiateViewController(withIdentifier: "Dash") as! DashboardViewController
+        account.modalPresentationStyle = .fullScreen
+        account.selectedIndex = 1
+        present(account, animated: true, completion: nil)
+
+    }
+    func addWishlistItemsToSubView(){
+                mainView.contentSize = CGSize(width: view.bounds.width, height: view.bounds.height + 800)
+
+        for i in 0...(WishListViewController.wishListArray.count-1){// change to cart items
+            mainView!.addSubview(WishListViewController.wishListArray[i])
+            mainView!.translatesAutoresizingMaskIntoConstraints = false//
+            mainView!.subviews[i+2].frame = CGRect(x: 20, y: (100 + 150 * (i)), width: Int(UIScreen.main.bounds.width) - 40, height: 130)
+//            view.subviews[i+1].subviews[0] = CGRect(x:300, y:150, width: view.subviews[i+1].frame.width, height: 100)
+//            mainView!.subviews[i+3].subviews[0].topAnchor.constraint(equalTo: mainView!.subviews[i+3].topAnchor, constant: 20).isActive = true
+//            mainView!.subviews[i+3].subviews[0].leadingAnchor.constraint(equalTo: mainView!.subviews[i+3].leadingAnchor, constant: 120).isActive = true
+
         }
 
-        tableView.rowHeight = 128
+        
     }
     
-    
-    
-    @IBAction func returnToProfile(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
-        // override viewDidAppear if any data needs to change and update because above line doesn't call viewDidLoad in view controller below
-    }
-    
-   
 
 }
+
+
--- Sahara/Dashboard/CartViewController.swift
+++ Sahara/Dashboard/CartViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 class CartViewController: UIViewController {
-    static var deletedItem: Bool = false
+    static var inst = CartViewController()
     static var cartArray: [cartItem] = []
     @IBOutlet weak var mainView: UIScrollView!
     @IBOutlet weak var deliveryAddress: UIButton!
@@ -55,31 +55,16 @@ class CartViewController: UIViewController {
             quan.font = UIFont(name: "arial", size: 25)
             quan.frame = CGRect(x: 120, y: 80, width: 10, height: 40)
             self.addSubview(quan)
-            let add = UIButton()
-            add.setTitle("+", for: .normal)
-            add.titleLabel?.font = UIFont(name: "arial", size: 30)
-            add.setTitleColor(.black, for: .normal)
-//            let cross = UIImage(systemName: "cross.fill")
-//            let crossTinted = cross?.withRenderingMode(.alwaysTemplate)
-//            add.setImage(crossTinted, for: .normal)
-//            add.tintColor = .black
-            add.frame = CGRect(x: 125, y: 80, width: 40, height: 40)
-            add.addTarget(self, action: #selector(hi), for: .touchUpInside)
-            self.addSubview(add)
             let delete = CustomButton()
             delete.setTitle("Delete", for: .normal)
             delete.frame = CGRect(x: 170, y: 75, width: 70, height: 50)
             delete.tag = index
-            
             delete.addTarget(self, action: #selector(deleteCartItem), for: .touchUpInside)
-            
             self.addSubview(delete)
             let price = UILabel()
             price.text = (String(cartItemPrice))
             price.frame = CGRect(x: 270, y: 60, width: 100, height: 20)
             self.addSubview(price)
-            self.sendSubviewToBack(add)
-            
             title.translatesAutoresizingMaskIntoConstraints = false
         }
         
@@ -87,32 +72,31 @@ class CartViewController: UIViewController {
             fatalError("init(coder:) has not been implemented")
         }
 
-        @objc func hi(words: String){
-            print(words)
-        }
         @objc func deleteCartItem(sender: UIButton){
-            print("hi")
             let user1 = DBHelper.inst.fetchUser(query: ud.string(forKey: "currUser")!)
             prod = DBHelper.inst.fetchProduct(id: user1!.cart![sender.tag])
             DBHelper.inst.removeFromCart(prodID: prod!.id!, uName: ud.string(forKey: "currUser")!)
             cartArray.remove(at: index)
-            CartViewController.deletedItem = true
-            print("hello")
+            CartViewController.inst.closeView()
         }
     }
+    func closeView(){
+        var sb = UIStoryboard(name: "Main", bundle: nil)
+
+        let cart = sb.instantiateViewController(withIdentifier: "Dash") as! DashboardViewController
+        cart.modalPresentationStyle = .fullScreen
+cart.selectedIndex = 2
+        present(cart, animated: true, completion: nil)
+    }
     func configureSubView(){
-//        addToCartButton()
         addCartItemsToSubView()
-//        setSubViewConstraints()
     }
 
-    func testCart(){
-        CartViewController.cartArray = []    }
     var titleLabel = UILabel()
     override func viewDidLoad() {
-//        testCart()
         
         super.viewDidLoad()
+        CartViewController.inst = self
         user = DBHelper.inst.fetchUser(query: ud.string(forKey: "currUser")!)
         
         if (ud.string(forKey: "currUser")!.hasPrefix("_")) {
@@ -129,74 +113,22 @@ class CartViewController: UIViewController {
         print(user!.cart!)
 
     }
-    override func viewWillAppear(_ animated: Bool) {
-        print(CartViewController.deletedItem)
-        if(CartViewController.deletedItem == true){
-            CartViewController.deletedItem = false
-            let sb : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-                        let cart = sb.instantiateViewController(withIdentifier: "Dash") as! DashboardViewController
-                        cart.modalPresentationStyle = .fullScreen
-            cart.selectedIndex = 2
-                        present(cart, animated: true, completion: nil)
-        }
-        
-    }
 
-//    func addToCartButton(){
-//        let button = CustomButton(frame: CGRect(x: 50, y: 175, width: mainView.bounds.width - 100, height: 50))
-//        button.setTitle("Proceed to checkout", for: .normal)
-//        mainView.addSubview(button)
-//        print(mainView.contentSize)
-//        mainView!.translatesAutoresizingMaskIntoConstraints = false//
-//        print(mainView.subviews)
-//    }
     func addCartItemsToSubView(){
                 mainView.contentSize = CGSize(width: view.bounds.width, height: view.bounds.height + 800)
-
-        for i in 0...CartViewController.cartArray.count-1{// change to cart items
+            
+        for i in 0...(CartViewController.cartArray.count-1){// change to cart items
             mainView!.addSubview(CartViewController.cartArray[i])
             mainView!.translatesAutoresizingMaskIntoConstraints = false//
             mainView!.subviews[i+2].frame = CGRect(x: 20, y: (100 + 150 * (i)), width: Int(UIScreen.main.bounds.width) - 40, height: 130)
-//            view.subviews[i+1].subviews[0] = CGRect(x:300, y:150, width: view.subviews[i+1].frame.width, height: 100)
-//            mainView!.subviews[i+3].subviews[0].topAnchor.constraint(equalTo: mainView!.subviews[i+3].topAnchor, constant: 20).isActive = true
-//            mainView!.subviews[i+3].subviews[0].leadingAnchor.constraint(equalTo: mainView!.subviews[i+3].leadingAnchor, constant: 120).isActive = true
 
         }
 
         
     }
-    func setSubViewConstraints(){
-        for i in 1...(mainView!.subviews.count - 1){
-            
-        }
-    func setCartItemConstraints(){
-        
-    }
-    
 
 }
 
-/*
- // Only override draw() if you perform custom drawing.
- // An empty implementation adversely affects performance during animation.
- override func draw(_ rect: CGRect) {
- // Drawing code
- }
- */
 
 
 
-
-
-/*
- // MARK: - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
- // Get the new view controller using segue.destination.
- // Pass the selected object to the new view controller.
- }
- */
-
-
-}
